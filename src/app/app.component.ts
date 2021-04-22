3d9882293bcb0c95ff974879d9a11b411733c67e
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-22 17:35:20 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  title = 'Temperature in Angular ' + VERSION.major;
+  title: string = 'Temperature in Angular ' + VERSION.major;
   cities = [
     {
       nome: 'Torino',
