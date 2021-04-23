37ac99b601e515d1fe953f3db31a3a8f269e3ad7
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-23 09:40:01 +0200

    1 file modified

--- src/app/notifica/notifica.component.ts
+++ src/app/notifica/notifica.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { tempCity } from '../app.component'
 
 @Component({
@@ -7,6 +7,7 @@ import { tempCity } from '../app.component'
   styleUrls: ['./notifica.component.css']
 })
 export class NotificaComponent implements OnInit {
+  @Input() selezione: tempCity;
   constructor() { }
   clean() {
     this.selezione.nome = undefined;
