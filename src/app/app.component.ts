63e52dd9a96e6e677d8e445091c492e3f56bae98
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-23 10:15:21 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component, VERSION } from '@angular/core';
-import { OpenweatherService } from './openweather.service';
+import { Component, VERSION } from "@angular/core";
+import { OpenweatherService } from "./openweather.service";
 
 export class tempCity {
   nome: string;
-  valore: string
+  valore: string;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  title: string = 'Temperature in Angular ' + VERSION.major;
+export class AppComponent {
+  title: string = "Temperature in Angular " + VERSION.major;
   selezione: tempCity;
-  cities: Array<string> = ['Torino','Milano','Genova'];
-  constructor(private ows: OpenweatherService) { }
+  cities: Array<string> = ["Torino", "Milano", "Genova"];
+  constructor(private ows: OpenweatherService) {}
   refreshTemperature(itemName: string) {
-    this.selezione = new tempCity;
+    this.selezione = new tempCity();
     this.selezione.nome = itemName;
-    this.ows.getData(this.selezione.nome).subscribe(
-      ( x: any ) => this.selezione.valore = x.data[0].temp,
-      err => console.error('Observer got an error: ' + err)
-    );
- }
+    this.ows
+      .getData(this.selezione.nome)
+      .subscribe(
+        (x: any) => (this.selezione.valore = x.data[0].temp),
+        err => console.error("Observer got an error: " + err)
+      );
+  }
+  addCity(newCity: string) {
+    this.cities.push(newCity);
+  }
 }
