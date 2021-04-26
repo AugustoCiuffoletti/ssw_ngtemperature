849aaa81c31ed4c67c033085cc4fe126e55bbda8
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-26 18:17:47 +0200

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,7 +34,7 @@ describe("AppComponent", () => {
     expect(app.title).toEqual("Temperature in Angular 11");
   });
 
-  it("la proprietà selezione dovrebbe essere indefinita", () => {
+  it("l'oggetto selezione dovrebbe essere indefinita", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.selezione).toBeUndefined();
