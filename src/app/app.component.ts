60ff5ca9e8b5b791a33bd3870f1523671fb89e0e
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-26 14:47:40 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component, VERSION } from '@angular/core';
 
 class tempCity {
   nome: string;
-  valore: string
+  valore: string;
+  constructor(nome: string, valore: string) {
+    this.nome=nome;
+    this.valore=valore;
+  }
 }
 
 @Component({
@@ -14,16 +18,9 @@ export class AppComponent  {
   title: string = 'Temperature in Angular ' + VERSION.major;
   selezionata: string;
   cities: Array<tempCity> = [
-    {
-      nome: 'Torino',
-      valore: '14'
-    }, {
-      nome: 'Milano',
-      valore: '15'
-    }, {
-      nome: 'Genova',
-      valore: '18'
-    }
+    new tempCity('Torino','14'),
+    new tempCity('Milano','15'), 
+    new tempCity('Genova','18')
   ];
   seleziona(name: string) {
 	  this.selezionata = name;
