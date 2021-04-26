04770c84adde3bc5a46a0abad83c66afd196bfec
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-26 15:11:13 +0200

    2 files modified

--- src/app/notifica/notifica.component.ts
+++ src/app/notifica/notifica.component.ts
@@ -7,10 +7,10 @@ import { tempCity } from '../app.component'
   styleUrls: ['./notifica.component.css']
 })
 export class NotificaComponent implements OnInit {
-  @Input() selezione: tempCity;
+  @Input() dato: tempCity;
   constructor() { }
   clean() {
-    this.selezione = undefined;
+    this.dato = undefined; // prima era 'selezione'
   }
   ngOnInit() {
   }
