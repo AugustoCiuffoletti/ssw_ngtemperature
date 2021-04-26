eb08bb47272eefcaf5619578cff33eca2b53063a
AugustoCiuffoletti <augusto@di.unipi.it>
2021-04-26 17:55:26 +0200

    1 file modified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,50 +1,43 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 import { OpenweatherService } from "./openweather.service";
 
-
-describe('Testing tests', () => {
-  it('Vero è vero', () => expect(true).toBeTrue());
-  it('Vero è falso', () => expect(true).toBeFalse());
+describe("Testing tests", () => {
+  it("Vero = vero", () => expect(true).toBeTrue());
+  it("Vero = falso", () => expect(true).toBeFalse());
 });
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule
-      ],
-      providers: [
-        AppComponent
-      ]
+      imports: [HttpClientModule],
+      providers: [AppComponent]
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeDefined();
   });
 
-
   it(`should have as title 'Temperature in Angular 11'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Temperature in Angular 11');
+    expect(app.title).toEqual("Temperature in Angular 11");
   });
 
-  it('la proprietà selezione dovrebbe essere indefinita', () => {
+  it("la proprietà selezione dovrebbe essere indefinita", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.selezione).toBeUndefined();
-});
+  });
 
-it('la proprietà cities dovrebbe contenere quella inserita con addCity', () => {
+  it("la proprietà cities dovrebbe contenere quella inserita con addCity", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     app.addCity("Verona");
-    expect(app.cities).toContain('Verona');
-});
-
+    expect(app.cities).toContain("Verona");
+  });
 });
